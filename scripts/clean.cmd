a2c46276b83112eb6f4675e7f049ed8dfabf9c14
Ropean <i9196880@gmail.com>
2025-08-31 14:05:43 -0400

    Enhanced clean script

--- scripts/clean.cmd
+++ scripts/clean.cmd
@@ -10,8 +10,9 @@ if exist CSharpHash\obj rmdir /s /q CSharpHash\obj
 echo Running dotnet clean...
 dotnet clean CSharpHash >nul 2>&1
 
+echo Removing any remaining build artifacts...
+for /r . %%d in (bin obj) do @if exist "%%d" rmdir /s /q "%%d" 2>nul
+
 echo Done.
 popd
 exit /b 0
-
-
